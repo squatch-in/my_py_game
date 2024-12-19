575fc0262b09ee1522bff2ddc1b1254ac7bd3213
squatch-in <jvwyatt&@gmail.com>
2024-12-19 09:26:44 -0600

    player and  movment added

--- main.py
+++ main.py
@@ -3,22 +3,29 @@
 # throught this file
 import pygame
 from constants import *
+from player import Player
 
-def starting():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
-starting()
-
-if __name__ == "__main__":
+def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
+#game loop
     while True:
-        screen.fill(color="black")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.QUIT()
-        
+                return
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        player.update(dt)
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
     main()
